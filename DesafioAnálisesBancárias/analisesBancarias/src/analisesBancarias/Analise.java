15c945027cfe9a3b6987886f2537a2c78fa1fddb
Gabrielgsw <ggermanow279@gmail.com>
2023-09-21 14:49:58 -0300

    adição desafio AnálisesBancárias

---
+++ DesafioAnálisesBancárias/analisesBancarias/src/analisesBancarias/Analise.java
@@ -0,0 +1,37 @@
+package analisesBancarias;
+
+import java.util.Date;
+
+class Analise {
+    Date data;
+    String descricao;
+
+    public Analise(Date data, String descricao) {
+      this.data = data;
+      this.descricao = descricao;
+    }
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public String toString() {
+		return descricao+"/n";
+	}
+    
+    
+	
+  }
